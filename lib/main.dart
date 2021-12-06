6e0146aeffe95294b4bc35eeb627e16443a926bf
Bui Duc Lam <buiduclam09@gmail.com>
2021-12-07 01:32:37 +0700

    set up app client

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:architecture_study/source/remote/app_client.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,15 +31,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
-    // TODO: implement initState
+    AppClient.shared
+        .initializeSubscriber(
+            deviceUUID: '123e4567-e89b-12d3-a456-426614174000')
+        .onError((error, stackTrace) => debugPrint('$error $stackTrace'))
+        .ignore();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter test')
-      ),
+      appBar: AppBar(title: const Text('Flutter test')),
       body: const Center(
         child: Text('Test restful api'),
       ),
--- lib/source/remote/app_client.dart
+++ lib/source/remote/app_client.dart
@@ -1,3 +1,75 @@
-class AppClient{
+import 'dart:convert';
+import 'dart:io';
 
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqlbrite/sqlbrite.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path/path.dart';
+
+Future<Database> _openDb() async {
+  final directory = await getApplicationDocumentsDirectory();
+  final path = join(directory.path, 'database.db');
+  return await openDatabase(
+    path,
+    version: 1,
+    onCreate: (db, version) {},
+  );
+}
+
+class AppClient {
+  static const _initializeSubscriberUrl =
+      'http://gusrix.software/api/sync/initializeSubscriber/';
+  static final shared = AppClient._(
+      http.Client(),
+      _openDb().then(
+            (value) => BriteDatabase(value, logger: debugPrint),
+      ));
+  final http.Client _client;
+  final Future<BriteDatabase> _db;
+
+  AppClient._(this._client, this._db);
+
+  Future<Map<String, String>> _getSqlCommands(String deviceUUID) async {
+    final response = await _client.get(
+        Uri.parse(_initializeSubscriberUrl + deviceUUID));
+    if (response.statusCode < 200 || response.statusCode >= 300) {
+      throw HttpException(
+          'code: ${response.statusCode}, body: ${response.body}',
+          uri: response.request?.url);
+    }
+    final decoded = jsonDecode(response.body) as Map<String, dynamic>;
+    if (!identical(decoded['success'], true)) {
+      throw HttpException(
+        (decoded['message'] as String?) ?? 'Failed request: ${response.body}',
+        uri: response.request?.url,
+      );
+    }
+    return Map<String, String>.from(jsonDecode(decoded['result'] as String));
+  }
+
+  Future<void> initializeSubscriber({required String deviceUUID}) async {
+    final db = await _db;
+    final sqlCommands = await _getSqlCommands(deviceUUID);
+    for (final entry in sqlCommands.entries) {
+      await db.execute(entry.value);
+      debugPrint('Executed successfully : ${entry.key}');
+    }
+
+    final tableNames = await db.query(
+      'sqlite_master',
+      columns: ['name'],
+      where: 'type = ?',
+      whereArgs: ['table'],
+      orderBy: 'name',
+    );
+    debugPrint(
+        'Tables: ${tableNames.map((e) => e['name']).where((element) =>
+        element != null).join(
+            '\n'
+        )}'
+    );
+    debugPrint('Successfully initialized subscriber');
+  }
 }
